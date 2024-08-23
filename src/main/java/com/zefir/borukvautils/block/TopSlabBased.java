d91155b7b0daa038779f39ad39ffe2f6011df6ac
Stikulzon <stickulzon@gmail.com>
2024-08-23 14:01:01 +0200

    - Updated to the lastest polymer version - Fixed 1.21.1 incapability

--- src/main/java/com/zefir/borukvautils/block/TopSlabBased.java
+++ src/main/java/com/zefir/borukvautils/block/TopSlabBased.java
@@ -3,12 +3,12 @@ package com.zefir.borukvautils.block;
 import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
 import net.minecraft.block.BlockState;
 
-import static com.zefir.borukvautils.data.Models.SLAB_BASED_STATE;
+import static com.zefir.borukvautils.data.Models.TOP_SLAB_BASED_STATE;
 
 // Use if you want to create a block with a hitbox.
-public interface SlabBased extends PolymerTexturedBlock {
+public interface TopSlabBased extends PolymerTexturedBlock {
     @Override
     default BlockState getPolymerBlockState(BlockState state) {
-        return SLAB_BASED_STATE;
+        return TOP_SLAB_BASED_STATE;
     }
 }
--- src/main/java/com/zefir/borukvautils/data/Models.java
+++ src/main/java/com/zefir/borukvautils/data/Models.java
@@ -1,6 +1,5 @@
 package com.zefir.borukvautils.data;
 
-import com.chocohead.mm.api.ClassTinkerers;
 import eu.pb4.polymer.blocks.api.BlockModelType;
 import eu.pb4.polymer.blocks.api.PolymerBlockModel;
 import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
@@ -14,15 +13,17 @@ public class Models {
     public static BlockState KELP_BASED_STATE;
     public static BlockState TRIPWIRE_BASED_STATE;
     public static BlockState FLAT_TRIPWIRE_BASED_STATE;
-    public static BlockState SLAB_BASED_STATE;
+    public static BlockState TOP_SLAB_BASED_STATE;
+    public static BlockState BOTTOM_SLAB_BASED_STATE;
 
     public static void init(){
-        PolymerBlockModel TRANSPARENT_BLOCK_MODEL = PolymerBlockModel.of(id("block/transparent_texture"));
+//        PolymerBlockModel TRANSPARENT_BLOCK_MODEL = PolymerBlockModel.of(id("block/transparent_texture"));
 
-        PLANT_BASED_STATE = PolymerBlockResourceUtils.requestBlock(BlockModelType.PLANT_BLOCK, TRANSPARENT_BLOCK_MODEL);
-        KELP_BASED_STATE = PolymerBlockResourceUtils.requestBlock(BlockModelType.KELP_BLOCK, TRANSPARENT_BLOCK_MODEL);
-        TRIPWIRE_BASED_STATE = PolymerBlockResourceUtils.requestBlock(ClassTinkerers.getEnum(BlockModelType.class, "TRIPWIRE_BLOCK"), TRANSPARENT_BLOCK_MODEL);
-        FLAT_TRIPWIRE_BASED_STATE = PolymerBlockResourceUtils.requestBlock(ClassTinkerers.getEnum(BlockModelType.class, "FLAT_TRIPWIRE_BLOCK"), TRANSPARENT_BLOCK_MODEL);
-        SLAB_BASED_STATE = PolymerBlockResourceUtils.requestBlock(ClassTinkerers.getEnum(BlockModelType.class, "SLAB_BLOCK"), TRANSPARENT_BLOCK_MODEL);
+        PLANT_BASED_STATE = PolymerBlockResourceUtils.requestEmpty(BlockModelType.PLANT_BLOCK);
+        KELP_BASED_STATE = PolymerBlockResourceUtils.requestEmpty(BlockModelType.KELP_BLOCK);
+        TRIPWIRE_BASED_STATE = PolymerBlockResourceUtils.requestEmpty(BlockModelType.TRIPWIRE_BLOCK);
+        FLAT_TRIPWIRE_BASED_STATE = PolymerBlockResourceUtils.requestEmpty(BlockModelType.TRIPWIRE_BLOCK_FLAT);
+        TOP_SLAB_BASED_STATE = PolymerBlockResourceUtils.requestEmpty(BlockModelType.TOP_SLAB);
+        BOTTOM_SLAB_BASED_STATE = PolymerBlockResourceUtils.requestEmpty(BlockModelType.TOP_SLAB);
     }
 }
